9a4e1d07383bd2c5d7129aaed0b26eae016662fc
Abdul Fattah Ismail <72433456+Abdulfi2@users.noreply.github.com>
2023-07-05 23:50:13 +0700

    first commit

---
+++ resources/js/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import Navbar from "@/Components/Navbar";
+import { Head } from "@inertiajs/react";
+
+export default function Homepage() {
+    return (
+        <>
+            <Head title="Qurban Serasi - Kolaborasi Qurban Untuk Negeri" />
+            <Navbar />
+        </>
+    );
+}
